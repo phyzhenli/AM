10dace2333332533432004313ef039eb29d501c5
phyzhenli <48823046+phyzhenli@users.noreply.github.com>
2022-07-11 16:03:04 +0800

    Update GA.m

--- software/GA.m
+++ software/GA.m
@@ -168,7 +168,7 @@ function [] = genCpp(g, h, l, lamb, file_number, result, dN, sign)
         mul_name = "signed_";
     end
 
-    mul_name = mul_name + string(g) + "x" + string(h) + "_l" + string(l) + "_lamb" + string(lamb) + "_" + num2str(file_number-1, '%0' + string(ceil(log10(dN))) + 'd');
+    mul_name = mul_name + string(g) + "x" + string(h) + "_l" + string(l) + "_lamb" + string(lamb) + "_" + num2str(file_number, '%0' + string(ceil(log10(dN))) + 'd');
 
     x_width = 2^( ceil( log2( g ) ) );
     y_width = 2^( ceil( log2( h ) ) );
